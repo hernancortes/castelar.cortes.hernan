f5a289d6db6d70e8dd4ad71b4209223990c84e23
HC <hernan.cortes@unahur.edu.ar>
2020-05-03 23:03:07 -0300

    Creo modelo Producto y los muestro en la Vista

---
+++ Solucion-Ej-HTML/HTML.Website/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTML.Website.Models
+{
+    public class Producto
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
