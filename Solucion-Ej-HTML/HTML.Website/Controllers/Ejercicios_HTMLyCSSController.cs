4291fb5feaa19c08a7c09f38056b27fdb715efe4
HC <hernan.cortes@unahur.edu.ar>
2020-05-24 00:36:56 -0300

    Agrego ejercicios de 7 al 11 - práctica HTML

--- Solucion-Ej-HTML/HTML.Website/Controllers/Ejercicios_HTMLyCSSController.cs
+++ Solucion-Ej-HTML/HTML.Website/Controllers/Ejercicios_HTMLyCSSController.cs
@@ -38,5 +38,30 @@ namespace HTML.Website.Controllers
         {
             return View();
         }
+
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio8()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio9()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio10()
+        {
+            return View();
+        }
+
+        public ActionResult Ejercicio11()
+        {
+            return View();
+        }
     }
 }
